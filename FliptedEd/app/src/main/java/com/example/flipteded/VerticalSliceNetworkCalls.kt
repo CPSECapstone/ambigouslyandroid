aa75e808c053cc0e5598ad74753a6c400e71bac8
Chloe Hebert <chhebert@calpoly.edu>
2021-02-13 14:13:04 -0800

    url

--- FliptedEd/app/src/main/java/com/example/flipteded/VerticalSliceNetworkCalls.kt
+++ FliptedEd/app/src/main/java/com/example/flipteded/VerticalSliceNetworkCalls.kt
@@ -14,7 +14,7 @@ class VerticalSliceNetworkCalls {
         suspend fun sendStringToAWS(context: Context, str: String) =
             suspendCoroutine<Unit> { cont ->
 
-                val url = "https://reqres.in/api/users"
+                val url = "https://h931pnb4pe.execute-api.us-east-2.amazonaws.com/test"
                 val stringRequest = StringRequest(Request.Method.POST, url,
                     Response.Listener<String> { response ->
                         cont.resume(Unit)
@@ -26,14 +26,14 @@ class VerticalSliceNetworkCalls {
 
         suspend fun getStringsFromAWS(context: Context) =
             suspendCoroutine<List<String>> { cont ->
-                val url = "https://reqres.in/api/users?page=2"
+                val url = "https://h931pnb4pe.execute-api.us-east-2.amazonaws.com/test"
 
                 val jsonArrayRequest = JsonObjectRequest(
                     Request.Method.GET,
                     url,
                     null,
                     Response.Listener { response ->
-                        val strList = response.getJSONArray("strings")
+                        val strList = response.getJSONArray("Items")
                         val returnList = ArrayList<String>()
                         for (i in 0..strList.length()) {
                             returnList[i] = strList.getString(i)
