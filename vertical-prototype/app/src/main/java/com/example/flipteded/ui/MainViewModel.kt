3ae7285c91c85f2bf4c403aba31ab8b6d86e8d5f
Destin Estrela <destin.estrela@unity3d.com>
2021-02-25 16:38:04 -0800

    apollo query vertical slice

--- vertical-prototype/app/src/main/java/com/example/flipteded/ui/MainViewModel.kt
+++ vertical-prototype/app/src/main/java/com/example/flipteded/ui/MainViewModel.kt
@@ -2,6 +2,11 @@ package com.example.flipteded.ui
 
 import android.app.Application
 import androidx.lifecycle.*
+import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.coroutines.await
+import com.apollographql.apollo.coroutines.toDeferred
+import com.apollographql.apollo.exception.ApolloException
+import com.example.LaunchDetailsQuery
 import com.example.flipteded.backend.VerticalSliceNetworkCalls
 import com.example.flipteded.backend.VolleyAwsInterface
 import com.example.flipteded.businesslogic.DataEntry
@@ -23,6 +28,10 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
     private val getAllEntriesUseCase = GetAllEntriesUseCase(repo)
     private val setEntryUseCase = SaveNewEntryUseCase(repo)
 
+    val apolloClient = ApolloClient.builder()
+        .serverUrl("https://apollo-fullstack-tutorial.herokuapp.com/graphql")
+        .build()
+
     init {
         viewModelScope.launch {
             while(true) {
@@ -36,6 +45,26 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
         viewModelScope.launch {
             setEntryUseCase.invoke(DataEntry(str))
             _data.value = getAllEntriesUseCase.invoke().map{ it.item }
+        }
+    }
+
+    fun apolloTest()
+    {
+        viewModelScope.launch {  val response = try {
+            apolloClient.query(LaunchDetailsQuery(id = "83")).await()
+        } catch (e: ApolloException) {
+            // handle protocol errors
+            return@launch
+        }
+
+            val launch = response.data?.launch
+            if (launch == null || response.hasErrors()) {
+                // handle application errors
+                return@launch
+            }
+
+            // launch now contains a typesafe model of your data
+            println("Launch site: ${launch!!.site}")
         }
     }
 }
