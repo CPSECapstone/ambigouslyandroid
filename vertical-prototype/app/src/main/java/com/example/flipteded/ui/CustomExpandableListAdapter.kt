189bea3bf3b0d015e37a2338fb3262862e3cf356
Chloe Hebert <chhebert@calpoly.edu>
2021-03-04 11:04:14 -0800

    expandable list view

---
+++ vertical-prototype/app/src/main/java/com/example/flipteded/ui/CustomExpandableListAdapter.kt
@@ -0,0 +1,83 @@
+package com.example.flipteded.ui
+import android.content.Context
+import android.graphics.Typeface
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseExpandableListAdapter
+import android.widget.TextView
+import java.util.HashMap
+import com.example.flipteded.R
+
+class CustomExpandableListAdapter internal constructor(
+    private val context: Context,
+    private val titleList: List<String>,
+    private val dataList: HashMap<String, List<String>>
+) : BaseExpandableListAdapter() {
+    override fun getChild(listPosition: Int, expandedListPosition: Int): Any {
+        return this.dataList[this.titleList[listPosition]]!![expandedListPosition]
+    }
+    override fun getChildId(listPosition: Int, expandedListPosition: Int): Long {
+        return expandedListPosition.toLong()
+    }
+    override fun getChildView(
+        listPosition: Int,
+        expandedListPosition: Int,
+        isLastChild: Boolean,
+        convertView: View?,
+        parent: ViewGroup
+    ): View {
+        var convertView = convertView
+        val expandedListText = getChild(listPosition, expandedListPosition) as String
+        if (convertView == null) {
+            val layoutInflater =
+                this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            if (isLastChild) {
+                convertView = layoutInflater.inflate(R.layout.fragment_mark_progress, null)
+            }
+            else {
+                //convertView = layoutInflater.inflate(R.layout.item_list, null)
+                convertView = layoutInflater.inflate(R.layout.fragment_mark_progress, null)
+            }
+        }
+        val expandedListTextView = convertView!!.findViewById<TextView>(R.id.info_text)
+        expandedListTextView.text = expandedListText
+        return convertView
+    }
+    override fun getChildrenCount(listPosition: Int): Int {
+        return this.dataList[this.titleList[listPosition]]!!.size
+    }
+    override fun getGroup(listPosition: Int): Any {
+        return this.titleList[listPosition]
+    }
+    override fun getGroupCount(): Int {
+        return this.titleList.size
+    }
+    override fun getGroupId(listPosition: Int): Long {
+        return listPosition.toLong()
+    }
+    override fun getGroupView(
+        listPosition: Int,
+        isExpanded: Boolean,
+        convertView: View?,
+        parent: ViewGroup
+    ): View {
+        var convertView = convertView
+        val listTitle = getGroup(listPosition) as String
+        if (convertView == null) {
+            val layoutInflater =
+                this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            convertView = layoutInflater.inflate(R.layout.fragment_mark_progress, null)
+        }
+        val listTitleTextView = convertView!!.findViewById<TextView>(R.id.info_text)
+        //listTitleTextView.setTypeface(null, Typeface.BOLD)
+        listTitleTextView.text = "?"
+        return convertView
+    }
+    override fun hasStableIds(): Boolean {
+        return false
+    }
+    override fun isChildSelectable(listPosition: Int, expandedListPosition: Int): Boolean {
+        return true
+    }
+}
--- vertical-prototype/app/src/main/java/com/example/flipteded/ui/ItemListActivity.kt
+++ vertical-prototype/app/src/main/java/com/example/flipteded/ui/ItemListActivity.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import androidx.lifecycle.Observer
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
+import android.widget.ExpandableListAdapter
+import android.widget.ExpandableListView
 import com.example.flipteded.R
 
 import kotlinx.android.synthetic.main.activity_item_list.*
@@ -21,24 +24,88 @@ import kotlinx.android.synthetic.main.item_list.*
 class ItemListActivity : AppCompatActivity() {
 
     private lateinit var viewModel : MainViewModel
+    private var expandableListView: ExpandableListView? = null
+    private var adapter: ExpandableListAdapter? = null
+    private var titleList: List<String>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_item_list)
+        setContentView(R.layout.fragment_exp_list)
 
         setSupportActionBar(toolbar)
         toolbar.title = title
 
+        expandableListView = findViewById(R.id.expandableList)
+
+        val expandableListDetail =
+            HashMap<String, List<String>>()
+        val myFavCricketPlayers: MutableList<String> =
+            ArrayList()
+        myFavCricketPlayers.add("MS.Dhoni")
+        myFavCricketPlayers.add("Sehwag")
+        myFavCricketPlayers.add("Shane Watson")
+        myFavCricketPlayers.add("Ricky Ponting")
+        myFavCricketPlayers.add("Shahid Afridi")
+        val myFavFootballPlayers: MutableList<String> = ArrayList()
+        myFavFootballPlayers.add("Cristiano Ronaldo")
+        myFavFootballPlayers.add("Lionel Messi")
+        myFavFootballPlayers.add("Gareth Bale")
+        myFavFootballPlayers.add("Neymar JR")
+        myFavFootballPlayers.add("David de Gea")
+        val myFavTennisPlayers: MutableList<String> = ArrayList()
+        myFavTennisPlayers.add("Roger Federer")
+        myFavTennisPlayers.add("Rafael Nadal")
+        myFavTennisPlayers.add("Andy Murray")
+        myFavTennisPlayers.add("Novak Jokovic")
+        myFavTennisPlayers.add("Sania Mirza")
+        expandableListDetail["CRICKET PLAYERS"] = myFavCricketPlayers
+        expandableListDetail["FOOTBALL PLAYERS"] = myFavFootballPlayers
+        expandableListDetail["TENNIS PLAYERS"] = myFavTennisPlayers
+
+        if (expandableListView != null) {
+            val listData = expandableListDetail
+            titleList = ArrayList(listData.keys)
+            adapter = CustomExpandableListAdapter(this, titleList as ArrayList<String>, listData)
+            expandableListView!!.setAdapter(adapter)
+            expandableListView!!.setOnGroupExpandListener { groupPosition ->
+                Toast.makeText(
+                    applicationContext,
+                    (titleList as ArrayList<String>)[groupPosition] + " List Expanded.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            expandableListView!!.setOnGroupCollapseListener { groupPosition ->
+                Toast.makeText(
+                    applicationContext,
+                    (titleList as ArrayList<String>)[groupPosition] + " List Collapsed.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            expandableListView!!.setOnChildClickListener { _, _, groupPosition, childPosition, _ ->
+                Toast.makeText(
+                    applicationContext,
+                    "Clicked: " + (titleList as ArrayList<String>)[groupPosition] + " -> " + listData[(
+                            titleList as
+                                    ArrayList<String>
+                            )
+                            [groupPosition]]!!.get(
+                        childPosition
+                    ),
+                    Toast.LENGTH_SHORT
+                ).show()
+                false
+            }
+        }
         viewModel = ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory.getInstance(application))[MainViewModel::class.java]
 
-        setupRecyclerView(item_list)
-
+        //setupRecyclerView(item_list)
+/*
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, AddStringFragment.newInstance())
                 .commitNow()
         }
-
+*/
     }
 
     private fun setupRecyclerView(recyclerView: RecyclerView) {
