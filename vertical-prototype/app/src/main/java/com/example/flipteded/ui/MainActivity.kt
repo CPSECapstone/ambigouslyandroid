fc6ae202fe7779c0948b724aef7e7ec95265cf39
Chelsea Campbell <cpcampbe@calpoly.edu>
2021-03-06 14:59:03 -0800

    set up navbar

--- vertical-prototype/app/src/main/java/com/example/flipteded/ui/MainActivity.kt
+++ vertical-prototype/app/src/main/java/com/example/flipteded/ui/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.flipteded.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.flipteded.R
+import com.google.android.material.tabs.TabItem
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +14,23 @@ class MainActivity : AppCompatActivity() {
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction().replace(R.id.main_view, StudentHomeFragment.newInstance()).commitNow()
         }
+
+        val tabLayout = findViewById<TabLayout>(R.id.navbar)
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                // Handle tab select
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                // Handle tab reselect
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                // Handle tab unselect
+            }
+        })
     }
+
+
 }
