01e7efefc36a43b7df9acf4db3ed21a34a983bc6
Chloe Hebert <chhebert@calpoly.edu>
2021-03-30 16:35:03 -0700

    list of questions

---
+++ flipted/app/src/main/java/edu/calpoly/flipted/businesslogic/mc_question/GetAllQuestions.kt
@@ -0,0 +1,15 @@
+package com.example.flipteded.businesslogic.mc_question
+
+import edu.calpoly.flipted.businesslogic.goals.Goal
+
+class GetAllQuestions {
+    fun getAllQuestions(): List<Question> {
+        val answerOne = Answer("atomos", 1, isCorrect = false, isChecked = false)
+        val answerTwo = Answer("neutron", 1, isCorrect = false, isChecked = false)
+        val answerThree = Answer("nucleus", 1, isCorrect = true, isChecked = false)
+        val answerFour = Answer("nucleon", 1, isCorrect = false, isChecked = false)
+        val answerList = mutableListOf<Answer>(answerOne, answerTwo, answerThree, answerFour)
+
+        return listOf(Question("The center of an atom is called the ______.", 1, answerList))
+    }
+}
---
+++ flipted/app/src/main/java/edu/calpoly/flipted/businesslogic/mc_question/McDataObjects.kt
@@ -0,0 +1,14 @@
+package com.example.flipteded.businesslogic.mc_question
+
+data class Question(
+    val title : String,
+    val uid : Int,
+    val answers: MutableList<Answer>
+)
+
+data class Answer(
+    val description : String,
+    val parentId : Int,
+    val isCorrect : Boolean,
+    var isChecked : Boolean
+)
