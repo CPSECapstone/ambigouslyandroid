138e1fa3a64a45ac62fac03ac2425dc00b142066
Jordan Powers <jordanpowers1227@gmail.com>
2021-04-13 16:37:18 -0700

    Starter code to populate ImageBlock

--- flipted/app/src/main/java/edu/calpoly/flipted/ui/tasks/viewholders/ImageBlockViewHolder.kt
+++ flipted/app/src/main/java/edu/calpoly/flipted/ui/tasks/viewholders/ImageBlockViewHolder.kt
@@ -1,10 +1,28 @@
 package edu.calpoly.flipted.ui.tasks.viewholders
 
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import edu.calpoly.flipted.R
+import edu.calpoly.flipted.businesslogic.tasks.data.blocks.ImageBlock
 import edu.calpoly.flipted.businesslogic.tasks.data.blocks.TaskBlock
 
 class ImageBlockViewHolder(view : View) : TaskBlockViewHolder(view) {
+    private val title : TextView = view.findViewById(R.id.task_image_block_title)
+    private val image : ImageView = view.findViewById(R.id.task_block_image)
+
+
     override fun bind(block: TaskBlock) {
-        // TODO
+        val imageBlock = block as ImageBlock
+
+        if(block.title != null) {
+            title.text = block.title
+        } else {
+            title.visibility = View.GONE
+        }
+
+        block.imageUrl
+        // Use picasso or glide to populate the image with the block.imageUrl
+        // image.setData( picasso.loadImageFromUrl(block.imageUrl))
     }
 }
