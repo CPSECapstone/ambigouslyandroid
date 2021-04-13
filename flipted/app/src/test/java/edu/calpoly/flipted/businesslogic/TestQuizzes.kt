e5cd7152318ea508962a3645ce7f9533cbef43c9
Chloe Hebert <chhebert@calpoly.edu>
2021-04-13 15:29:04 -0700

    TestQuizzes

--- flipted/app/src/test/java/edu/calpoly/flipted/businesslogic/TestQuizzes.kt
+++ flipted/app/src/test/java/edu/calpoly/flipted/businesslogic/TestQuizzes.kt
@@ -1,4 +1,25 @@
 package edu.calpoly.flipted.businesslogic
 
+import edu.calpoly.flipted.businesslogic.mc_question.*
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Test
+import org.junit.Assert.*
+import java.util.*
+
 class TestQuizzes {
+    class MockQuizRepo : QuestionsRepo {
+        val data = mutableListOf<McQuestion>()
+        override suspend fun getAllQuestions(id: String) = data
+    }
+
+
+    @Test
+    fun getAllQuestionsTest() = runBlockingTest {
+        val repo = TestQuizzes.MockQuizRepo()
+        val useCase = GetAllQuestions(repo)
+
+        repo.data.add()
+
+
+    }
 }
