efb9f4eb505b710e3db17463d2fa314bdf760913
Jordan Powers <jordanpowers1227@gmail.com>
2021-04-12 18:49:36 -0700

    Add Quiz data objects

---
+++ flipted/app/src/main/java/edu/calpoly/flipted/businesslogic/quizzes/data/answers/FreeResponseAnswer.kt
@@ -0,0 +1,5 @@
+package edu.calpoly.flipted.businesslogic.quizzes.data.answers
+
+class FreeResponseAnswer(
+        val response: String
+) : AnswerType()
--- flipted/app/src/main/java/edu/calpoly/flipted/businesslogic/tasks/data/blocks/QuizBlock.kt
+++ flipted/app/src/main/java/edu/calpoly/flipted/businesslogic/tasks/data/blocks/QuizBlock.kt
@@ -1,10 +1,11 @@
 package edu.calpoly.flipted.businesslogic.tasks.data.blocks
 
+import edu.calpoly.flipted.businesslogic.quizzes.data.questions.Question
 import edu.calpoly.flipted.businesslogic.tasks.data.RubricRequirement
 
-class QuestionBlock(
-        val question: String,
-        val uid: Integer,
+class QuizBlock(
+        val questions: List<Question>,
+        val requiredQuestionsCorrect: Int,
         requirement: RubricRequirement,
         title: String?
 ) : TaskBlock(requirement, title)
