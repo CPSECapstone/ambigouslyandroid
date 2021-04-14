ae2a8871b13cf0ed87848dfc28d8939a4ab0ee8a
Jordan Powers <jordanpowers1227@gmail.com>
2021-04-14 16:37:56 -0700

    Integrate rubric into the tasks pager fragment

--- flipted/app/src/main/java/edu/calpoly/flipted/ui/tasks/rubric/TaskRubricFragment.kt
+++ flipted/app/src/main/java/edu/calpoly/flipted/ui/tasks/rubric/TaskRubricFragment.kt
@@ -5,23 +5,78 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.CheckBox
+import android.widget.ListView
+import androidx.lifecycle.ViewModelProvider
 import edu.calpoly.flipted.R
+import edu.calpoly.flipted.businesslogic.tasks.data.RubricRequirement
+import edu.calpoly.flipted.ui.tasks.TaskViewModel
+
 
 class TaskRubricFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var viewModel : TaskViewModel
+    private lateinit var list : ListView
+
+    private lateinit var adapter : RubricListAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.task_rubric_fragment, container, false)
+                              savedInstanceState: Bundle?): View?
+        = inflater.inflate(R.layout.task_rubric_fragment, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        list = view.findViewById(R.id.rubric_items_list)
+
+        viewModel = ViewModelProvider(requireActivity())[TaskViewModel::class.java]
+
+        val task = viewModel.currTask.value ?: throw IllegalArgumentException("Null task")
+
+        val rubricRequiments = task.pages.flatMap { page ->
+            page.blocks.map { block -> block.requirement }
+        }.filterNotNull()
+
+        adapter = RubricListAdapter()
+
+        list.adapter = adapter
+
+        adapter.data = rubricRequiments
+
     }
 
     companion object {
         @JvmStatic
-        fun newInstance() =
-                TaskRubricFragment()
+        fun newInstance() = TaskRubricFragment()
+    }
+
+
+    inner class RubricListAdapter : BaseAdapter() {
+
+        var data : List<RubricRequirement> = listOf()
+            set(value) {
+                field = value
+                notifyDataSetChanged()
+            }
+
+        override fun getCount(): Int = data.size
+
+        override fun getItem(p0: Int): RubricRequirement = data[p0]
+
+        override fun getItemId(p0: Int): Long = data[p0].uid.toLong()
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            val fillInView = convertView ?: layoutInflater.inflate(R.layout.task_rubric_checkbox, parent, false)
+            val checkBox : CheckBox = fillInView.findViewById(R.id.checkBox)
+
+            val data = getItem(position)
+
+            checkBox.text = data.description
+            checkBox.isChecked = data.isComplete
+
+            return fillInView
+        }
+
     }
 }
