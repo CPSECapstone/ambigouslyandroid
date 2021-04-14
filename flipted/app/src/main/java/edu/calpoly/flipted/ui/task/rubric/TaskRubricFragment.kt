add90d8ae1e7f7d6f926f5a2a288071c87f01015
Chelsea Campbell <cpcampbe@calpoly.edu>
2021-04-13 17:07:14 -0700

    Add basic task rubric xml

---
+++ flipted/app/src/main/java/edu/calpoly/flipted/ui/task/rubric/TaskRubricFragment.kt
@@ -0,0 +1,27 @@
+package edu.calpoly.flipted.ui.task.rubric
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import edu.calpoly.flipted.R
+
+class TaskRubricFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_task_rubric, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() =
+                TaskRubricFragment()
+    }
+}
