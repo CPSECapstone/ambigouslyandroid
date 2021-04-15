234ef04ed01d26e0e5af7bc81dfbbc6715059b6e
Jordan Powers <jordanpowers1227@gmail.com>
2021-04-14 17:33:55 -0700

    Fix broken unit tests

--- flipted/app/src/test/java/edu/calpoly/flipted/businesslogic/TestURLtoID.kt
+++ flipted/app/src/test/java/edu/calpoly/flipted/businesslogic/TestURLtoID.kt
@@ -12,13 +12,13 @@ class TestURLtoID {
 fun urlNonHttps() {
 
     val url1 = "youtu.be/1FJHYqE0RDg"
-    assertEquals("1FJHYqE0RDg", GetId.Idinput.getYouTubeId(url1))
+    assertEquals("1FJHYqE0RDg", GetId.getYouTubeId(url1))
 
     val url2 = "youtu.be/gdevNBJZYgI"
-    assertEquals("gdevNBJZYgI", GetId.Idinput.getYouTubeId(url2))
+    assertEquals("gdevNBJZYgI", GetId.getYouTubeId(url2))
 
     val url3 = "youtu.be/BwtQZO0vi24"
-    assertEquals("BwtQZO0vi24", GetId.Idinput.getYouTubeId(url3))
+    assertEquals("BwtQZO0vi24", GetId.getYouTubeId(url3))
 
 }
 
@@ -27,13 +27,13 @@ fun urlNonHttps() {
 fun urlYoutube() {
 
     val url1 = "https://youtu.be/1FJHYqE0RDg"
-    assertEquals("1FJHYqE0RDg", GetId.Idinput.getYouTubeId(url1))
+    assertEquals("1FJHYqE0RDg", GetId.getYouTubeId(url1))
 
     val url2 = "https://youtu.be/gdevNBJZYgI"
-    assertEquals("gdevNBJZYgI", GetId.Idinput.getYouTubeId(url2))
+    assertEquals("gdevNBJZYgI", GetId.getYouTubeId(url2))
 
     val url3 = "https://youtu.be/BwtQZO0vi24"
-    assertEquals("BwtQZO0vi24", GetId.Idinput.getYouTubeId(url3))
+    assertEquals("BwtQZO0vi24", GetId.getYouTubeId(url3))
 
 }
 
@@ -41,13 +41,13 @@ fun urlYoutube() {
 fun urlWatchVEqual() {
 
     val url1 = "https://www.youtube.com/watch?v=R3XpjB3P9WU"
-    assertEquals("R3XpjB3P9WU", GetId.Idinput.getYouTubeId(url1))
+    assertEquals("R3XpjB3P9WU", GetId.getYouTubeId(url1))
 
     val url2 = "https://www.youtube.com/watch?v=4AO-NBTJr5E"
-    assertEquals("4AO-NBTJr5E", GetId.Idinput.getYouTubeId(url2))
+    assertEquals("4AO-NBTJr5E", GetId.getYouTubeId(url2))
 
     val url3 = "https://www.youtube.com/watch?v=BwtQZO0vi24"
-    assertEquals("BwtQZO0vi24", GetId.Idinput.getYouTubeId(url3))
+    assertEquals("BwtQZO0vi24", GetId.getYouTubeId(url3))
 
 }
 
@@ -55,16 +55,18 @@ fun urlWatchVEqual() {
 fun urlWatchAnd() {
 
     val url1 = "https://www.youtube.com/watch?v=tV36QsE7JyM&t=235s"
-    assertEquals("tV36QsE7JyM", GetId.Idinput.getYouTubeId(url1))
+    assertEquals("tV36QsE7JyM", GetId.getYouTubeId(url1))
 
     val url2 = "https://www.youtube.com/watch?v=4AO-NBTJr5E&t=27s"
-    assertEquals("4AO-NBTJr5E", GetId.Idinput.getYouTubeId(url2))
+    assertEquals("4AO-NBTJr5E", GetId.getYouTubeId(url2))
 
     val url3 = "https://www.youtube.com/watch?v=R3XpjB3P9WU&t=349s"
-    assertEquals("R3XpjB3P9WU", GetId.Idinput.getYouTubeId(url3))
+    assertEquals("R3XpjB3P9WU", GetId.getYouTubeId(url3))
 
     val url4 = "https://www.youtube.com/watch?v=fsM-rOZI3TY&list=RDfsM-rOZI3TY&start_radio=1"
-    assertEquals("fsM-rOZI3TY", GetId.Idinput.getYouTubeId(url4))
+    assertEquals("fsM-rOZI3TY", GetId.getYouTubeId(url4))
+
+    assertEquals("fsM-rOZI3TY", GetId.getYouTubeId("https://www.youtube.com/watch?list=RDfsM-rOZI3TY&v=fsM-rOZI3TY&start_radio=1"))
 }
 
 
@@ -72,10 +74,10 @@ fun urlWatchAnd() {
 fun urlNonYoutube() {
 
     val url1 = "https://en.wikipedia.org/wiki/Main_Page"
-    assertEquals("Not YouTube Video", GetId.Idinput.getYouTubeId(url1))
+    assertEquals("Not YouTube Video", GetId.getYouTubeId(url1))
 
     val url2 = "https://vimeo.com/42647970"
-    assertEquals("Not YouTube Video", GetId.Idinput.getYouTubeId(url2))
+    assertEquals("Not YouTube Video", GetId.getYouTubeId(url2))
 
 }
 
@@ -84,13 +86,13 @@ fun urlNonYoutube() {
 fun JustId() {
 
     val url1 = "1FJHYqE0RDg"
-    assertEquals("1FJHYqE0RDg", GetId.Idinput.getYouTubeId(url1))
+    assertEquals("1FJHYqE0RDg", GetId.getYouTubeId(url1))
 
     val url2 = "gdevNBJZYgI"
-    assertEquals("gdevNBJZYgI", GetId.Idinput.getYouTubeId(url2))
+    assertEquals("gdevNBJZYgI", GetId.getYouTubeId(url2))
 
     val url3 = "BwtQZO0vi24"
-    assertEquals("BwtQZO0vi24", GetId.Idinput.getYouTubeId(url3))
+    assertEquals("BwtQZO0vi24", GetId.getYouTubeId(url3))
 
 }
 
