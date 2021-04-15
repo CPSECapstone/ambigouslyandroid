cae7b79d26b11575e3fa815f271ef9702893133f
Chelsea Campbell <cpcampbe@calpoly.edu>
2021-04-15 13:28:37 -0700

    UI Fragment Button Test

--- flipted/app/src/androidTest/java/edu/calpoly/flipted/ActivityInputOutputTest.kt
+++ flipted/app/src/androidTest/java/edu/calpoly/flipted/ActivityInputOutputTest.kt
@@ -1,18 +1,31 @@
 package edu.calpoly.flipted
 
 import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.navigation.Navigation
+import androidx.navigation.testing.TestNavHostController
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.ViewAssertion
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import edu.calpoly.flipted.ui.MainActivity
+import edu.calpoly.flipted.ui.goals.GoalsFragment
+import edu.calpoly.flipted.ui.home.StudentHomeFragment
+import edu.calpoly.flipted.ui.tasks.TaskPageFragment
+import edu.calpoly.flipted.ui.tasks.rubric.TaskRubricFragment
+import org.hamcrest.CoreMatchers.allOf
+import org.hamcrest.CoreMatchers.equalTo
 import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.util.regex.Pattern.matches
 
 
 /**
@@ -44,7 +57,14 @@ class ActivityInputOutputTest {
         //assertEquals(StudentHomeFragment,)
     }
 
+    @Test fun testStudentHomeFragment() {
+        val scenario = launchFragmentInContainer<StudentHomeFragment>()
+        onView(allOf(withId(R.id.goals_button), withText("Goals")))
+        onView(allOf(withId(R.id.quiz_button), withText("Take Quiz")))
+    }
 
-
-
+    @Test fun testGoalsFragment() {
+        val scenario = launchFragmentInContainer<GoalsFragment>()
+        onView(allOf(withId(R.id.newGoalButton), withText("Add New Goal")))
+    }
 }
