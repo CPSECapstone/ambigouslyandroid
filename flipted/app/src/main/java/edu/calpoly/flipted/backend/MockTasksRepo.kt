7a6e4956b9320213fd100e858477b7cee060dae8
Jordan Powers <jordanpowers1227@gmail.com>
2021-04-28 18:13:31 -0700

    Merge MockTasksRepo2 into MockTasksRepo

--- flipted/app/src/main/java/edu/calpoly/flipted/backend/MockTasksRepo.kt
+++ flipted/app/src/main/java/edu/calpoly/flipted/backend/MockTasksRepo.kt
@@ -83,23 +83,88 @@ class MockTasksRepo : TasksRepo {
         ), "c5110abd8c4", "Learn about the Test Bus", "Complete this task",
         10, null, null, dateFormat.parse("4-25-2021"), "", 0, null)
 
+    private val mockedTask2 = Task(
+        listOf(
+            Page(listOf(
+                TextBlock("What is a Test Driven Development?", 36),
+                TextBlock("""
+                    Test Driven Development (TDD) is both an analysis and design technique. 
+                    It is when you write the test cases for code before writing the code 
+                    itself. It helps us decide what we are programming and what we aren't 
+                    programming. Picking what's in scope and, more importantly what's out 
+                    of scope is critical to software development.
+                """.trimIndent(), 18),
+                TextBlock("Look at This!", 36),
+                ImageBlock("https://marsner.com/wp-content/uploads/test-driven-development-TDD.png"),
+                VideoBlock("https://www.youtube.com/watch?v=uGaNkTahrIw", "Watch this video!"),
+                TextBlock("Check your knowledge", 24),
+                QuizBlock(
+                    listOf(
+                        MultipleChoiceQuestion(
+                            listOf(
+                                MultipleChoiceAnswerOption("4", uid),
+                                MultipleChoiceAnswerOption("5", uid),
+                                MultipleChoiceAnswerOption("3", uid)
+                            ),"There are ___ stages of TDD.", 2, uids, null),
+                        MultipleChoiceQuestion(
+                            listOf(
+                                MultipleChoiceAnswerOption("Allows for repeatable tests", uid),
+                                MultipleChoiceAnswerOption("Enables rapid change", uid),
+                                MultipleChoiceAnswerOption("Provides documentation for different team members", uid),
+                                MultipleChoiceAnswerOption("It is faster then coding without TDD", uid),
+                                MultipleChoiceAnswerOption("Keeps code clear, simple, and testable", uid)
+                            ), "Which of the following is NOT a benefit of TDD?", 2, uids, null)
+                    ), 2,  uids, 5)
+                ),false), Page(listOf(
+                TextBlock("Better Design", 36),
+                TextBlock("""
+                    We saw how test-first drives design decisions. Not only do the decisions come 
+                    at a different time, the decisions themselves are different. Test-first code 
+                    tends to be more cohesive and less coupled than code in which testing isn’t 
+                    part of the intimate coding cycle. Some people worry that they can’t even get 
+                    their code done, and now they have to test, too. You don’t have to be a great 
+                    designer—you just have to be creatively lazy. 
+                """.trimIndent(), 18),
+                TextBlock("Show your understanding", 36),
+                QuizBlock(
+                    listOf(
+                        FreeResponseQuestion("""
+                            In your own words, summarize
+                            what is TDD and why it is useful.
+                        """.trimIndent(), 6, uids, null)),
+                    0, uids, 5)
+            ), false)
+        ), listOf(
+            RubricRequirement("Read about TDD", false, uids),
+            RubricRequirement("Watch TDD video", false, uids),
+            RubricRequirement("Answer multiple choice questions", false, uids),
+            RubricRequirement("Complete free-response question", false, uids)
+        ), "2", "What is TDD?",
+            "Read this information carefully and answer the provided questions",
+            10, null, null, dateFormat.parse("4-29-2021")!!,
+            "", 0, null)
+
+    private val savedTasks: Map<String, Task> = mapOf(
+            mockedTask.uid to mockedTask,
+            mockedTask2.uid to mockedTask
+    )
+
     private var savedProgress: MutableSet<String> = mutableSetOf()
 
     private var savedQuestionAnswers: MutableMap<String, StudentAnswerInput> = mutableMapOf()
 
 
-    override suspend fun getTask(taskId: String) : Task {
-        if(taskId != mockedTask.uid)
-            throw IllegalArgumentException("No task with $taskId exists")
-        return Task(mockedTask.pages, mockedTask.requirements.map {
-            if(it.uid in savedProgress) it.completed else it.incompleted
-        }, mockedTask.uid, mockedTask.name, mockedTask.instructions,
-        mockedTask.points, mockedTask.startAt, mockedTask.endAt,
-        mockedTask.dueDate, mockedTask.parentMissionId, mockedTask.parentMissionIndex, mockedTask.objectiveId)
-    }
+    override suspend fun getTask(taskId: String) : Task
+        = savedTasks[taskId]?.let { task ->
+            Task(task.pages, task.requirements.map {
+                if (it.uid in savedProgress) it.completed else it.incompleted
+            }, task.uid, task.name, task.instructions, task.points, task.startAt,
+            task.endAt, task.dueDate, task.parentMissionId, task.parentMissionIndex, task.objectiveId)
+        } ?: throw IllegalArgumentException("No task with $taskId exists")
+
 
     override suspend fun saveRubricProgress(progress: TaskRubricProgress) {
-        if(progress.task.uid != mockedTask.uid)
+        if(!savedTasks.containsKey(progress.task.uid))
             throw IllegalArgumentException("No task with ${progress.task.uid} exists")
         progress.finishedRequirements.forEach{
             savedProgress.add(it.uid)
@@ -107,7 +172,7 @@ class MockTasksRepo : TasksRepo {
     }
 
     override suspend fun saveQuizAnswer(answer: TaskQuizAnswer) {
-        if(answer.task.uid != mockedTask.uid)
+        if(!savedTasks.containsKey(answer.task.uid))
             throw IllegalArgumentException("No task with ${answer.task.uid} exists")
         savedQuestionAnswers[answer.answer.questionId] = answer.answer
     }
