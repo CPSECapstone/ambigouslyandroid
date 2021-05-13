daf6a2fdba7f29992ee5ac540412bb8cec0fbc9b
Chelsea Campbell <cpcampbe@calpoly.edu>
2021-05-13 00:21:25 -0700

    code review changes

--- flipted/app/src/main/java/edu/calpoly/flipted/ui/missions/MissionFragment.kt
+++ flipted/app/src/main/java/edu/calpoly/flipted/ui/missions/MissionFragment.kt
@@ -5,18 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.commit
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
-import com.amplifyframework.auth.AuthUserAttributeKey
 import edu.calpoly.flipted.R
-import edu.calpoly.flipted.ui.goals.GoalsFragment
-import edu.calpoly.flipted.ui.login.LoginFragment
-import edu.calpoly.flipted.ui.login.LoginViewModel
-import edu.calpoly.flipted.ui.tasks.TaskFragment
 
 /**
  * A simple [Fragment] subclass.
@@ -25,21 +15,15 @@ import edu.calpoly.flipted.ui.tasks.TaskFragment
  */
 class MissionFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? =
+            inflater.inflate(R.layout.fragment_mission, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val recyclerView : RecyclerView = view.findViewById(R.id.fragment_mission_recycler_view)
         val adapter = MissionsRecyclerViewAdapter(requireActivity())
         recyclerView.adapter = adapter
-    }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mission, container, false)
     }
 
     companion object {
