57133afb80f9ad0a995166d8e421f4c141c76d2e
Jordan Powers <jordanpowers1227@gmail.com>
2021-05-16 12:54:57 -0700

    Improve updateSchema.py script

--- flipted/updateSchema.py
+++ flipted/updateSchema.py
@@ -1,4 +1,45 @@
+from pathlib import Path
+import os
+import argparse
 import subprocess
-URL = "https://knyio2nl7d.execute-api.us-east-1.amazonaws.com/dev/graphql"
+from sys import platform
+import re
+import shutil
 
-subprocess.run(["gradlew.bat", "downloadApolloSchema", "--schema=app/src/main/graphql/edu/calpoly/flipted/schema.json", "--endpoint={}".format(URL)], shell=True)
+script_dir = Path(__file__).resolve().parent
+os.chdir(script_dir)
+
+URLS = {
+    "prod": "https://knyio2nl7d.execute-api.us-east-1.amazonaws.com/dev/graphql",
+    "dev": "https://18wi8h43il.execute-api.us-east-1.amazonaws.com/dev-flipted/graphql"
+}
+
+parser = argparse.ArgumentParser(description='Retrieve the most up-to-date Apollo GraphQL schema')
+parser.add_argument('--env', choices=['prod', 'dev'], default='dev')
+args = parser.parse_args()
+
+url = URLS[args.env]
+
+if platform == 'win32':
+    gradlew = 'gradlew.bat'
+else:
+    gradlew = './gradlew'
+
+print("Downloading {} schema ({})".format(args.env, url))
+subprocess.run([gradlew, "downloadApolloSchema", "--schema=app/src/main/graphql/edu/calpoly/flipted/schema.json", "--endpoint={}".format(url)], shell=True)
+
+apolloRepo = Path('./app/src/main/java/edu/calpoly/flipted/backend/ApolloRepo.kt')
+tmpApolloRepo = apolloRepo.parent / (apolloRepo.name + '.tmp')
+
+expr = re.compile('^(\s+const val BACKEND_URL = )"[a-zA-Z0-9\.:\/-]+"\s*$')
+
+with apolloRepo.open('r') as arf, tmpApolloRepo.open('w') as tf:
+    for l in arf.readlines():
+        match = expr.fullmatch(l)
+        if match:
+            tf.write("{}\"{}\"\n".format(match.group(1), url))
+        else:
+            tf.write(l)
+
+apolloRepo.unlink()
+shutil.move(tmpApolloRepo, apolloRepo)
