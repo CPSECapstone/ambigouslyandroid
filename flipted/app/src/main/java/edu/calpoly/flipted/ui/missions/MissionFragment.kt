783d0a56b897a565900013b7104f24feab3a59c8
Jordan Powers <jordanpowers1227@gmail.com>
2021-05-26 16:22:04 -0700

    Toast if mission fails to load

--- flipted/app/src/main/java/edu/calpoly/flipted/ui/missions/MissionFragment.kt
+++ flipted/app/src/main/java/edu/calpoly/flipted/ui/missions/MissionFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -48,7 +49,10 @@ class MissionFragment : Fragment() {
         viewModel = ViewModelProvider(requireActivity())[MissionsViewModel::class.java]
 
         viewModel.missionsProgress.observe(viewLifecycleOwner, Observer {
-            adapter.data = it[missionId]
+            val mission = it[missionId]
+            if(mission == null)
+                Toast.makeText(requireActivity(), "Failed to retrieve mission!", Toast.LENGTH_LONG).show()
+            adapter.data = mission
             adapter.notifyDataSetChanged()
         })
 
