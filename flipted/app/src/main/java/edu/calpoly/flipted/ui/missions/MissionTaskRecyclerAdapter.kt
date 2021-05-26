fe4bac04c7353c060d6f968027cb76dbabeaea8b
Jordan Powers <jordanpowers1227@gmail.com>
2021-05-25 17:20:10 -0700

    Allow for selecting items

--- flipted/app/src/main/java/edu/calpoly/flipted/ui/missions/MissionTaskRecyclerAdapter.kt
+++ flipted/app/src/main/java/edu/calpoly/flipted/ui/missions/MissionTaskRecyclerAdapter.kt
@@ -1,10 +1,10 @@
 package edu.calpoly.flipted.ui.missions
 
-import android.graphics.drawable.LayerDrawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
@@ -25,12 +25,31 @@ class MissionTitleViewHolder(view: View) : MissionTaskViewHolder(view) {
     }
 }
 
-class TaskViewHolder(view: View, private val fragment: Fragment) : MissionTaskViewHolder(view) {
+class TaskViewHolder(view: View, private val fragment: Fragment, private val adapter: MissionTaskRecyclerAdapter)
+    : MissionTaskViewHolder(view), View.OnClickListener {
     private val indicator: ImageView = view.findViewById(R.id.mission_task_item_indicator)
     private val title: TextView = view.findViewById(R.id.mission_task_item_title)
     private val points: TextView = view.findViewById(R.id.mission_task_item_points)
 
-    fun bind(task: TaskStats) {
+    private var pos : Int? = null
+    private lateinit var taskId: String
+
+    init {
+        view.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        adapter.selectedIdx = pos
+        //TODO: Load the sidebar with the selected item
+        /*
+            fragment.childFragmentManager.commit {
+                replace(R.id.<sidebar_container_id>, <sidebar fragment>.newInstance(taskId))
+                setReorderingAllowed(true)
+            }
+         */
+    }
+
+    fun bind(task: TaskStats, pos: Int) {
         title.text = task.task.name
 
         val indicatorDrawable = ResourcesCompat.getDrawable(fragment.resources,
@@ -43,16 +62,45 @@ class TaskViewHolder(view: View, private val fragment: Fragment) : MissionTaskVi
         indicator.setImageDrawable(indicatorDrawable)
 
         points.text = "${task.task.points} points"
+
+        this.pos = pos
+        taskId = task.task.id
     }
+}
+
+class SelectedTaskViewHolder(view: View) : MissionTaskViewHolder(view) {
+    private val title: TextView = view.findViewById(R.id.mission_selected_item_title)
+    private val points: TextView = view.findViewById(R.id.mission_selected_item_points)
+    private val progress: ProgressBar = view.findViewById(R.id.mission_selected_item_progress)
+
+    fun bind(task: TaskStats) {
+        title.text = task.task.name
+        points.text = "${task.task.points} points"
+
+        progress.max = task.task.points
+        progress.progress = task.submission?.pointsAwarded ?: 0
+    }
+
 }
 
 class MissionTaskRecyclerAdapter(private val fragment: Fragment) : RecyclerView.Adapter<MissionTaskViewHolder>() {
     var data : MissionProgress? = null
+    var selectedIdx: Int? = null
+        set(value) {
+            val oldVal = field
+            field = value
+            if(oldVal != null)
+                notifyItemChanged(oldVal)
+            if(value != null)
+                notifyItemChanged(value)
+        }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MissionTaskViewHolder {
         val view = LayoutInflater.from(fragment.requireActivity()).inflate(viewType, parent, false)
         return when(viewType) {
             R.layout.mission_title_item -> MissionTitleViewHolder(view)
-            R.layout.mission_task_item -> TaskViewHolder(view, fragment)
+            R.layout.mission_task_item -> TaskViewHolder(view, fragment, this)
+            R.layout.mission_selected_item -> SelectedTaskViewHolder(view)
             else -> throw IllegalArgumentException("Unknown viewType")
         }
     }
@@ -61,7 +109,8 @@ class MissionTaskRecyclerAdapter(private val fragment: Fragment) : RecyclerView.
         val mission = data ?: return
         when(holder) {
             is MissionTitleViewHolder -> holder.bind(mission)
-            is TaskViewHolder -> holder.bind(mission.progress[position - 1])
+            is TaskViewHolder -> holder.bind(mission.progress[position - 1], position)
+            is SelectedTaskViewHolder -> holder.bind(mission.progress[position - 1])
         }
     }
 
@@ -69,6 +118,7 @@ class MissionTaskRecyclerAdapter(private val fragment: Fragment) : RecyclerView.
 
     override fun getItemViewType(position: Int): Int = when(position) {
         0 -> R.layout.mission_title_item
+        selectedIdx -> R.layout.mission_selected_item
         else -> R.layout.mission_task_item
     }
 }
