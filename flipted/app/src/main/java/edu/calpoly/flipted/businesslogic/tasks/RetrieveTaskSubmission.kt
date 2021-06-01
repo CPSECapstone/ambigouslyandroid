2145ad83d976d771721429a3fecec41b94a3166c
Destin Estrela <destin.estrela@unity3d.com>
2021-06-01 16:25:44 -0700

    Fixed accidentally submitting task instead of retrieving submission

--- flipted/app/src/main/java/edu/calpoly/flipted/businesslogic/tasks/RetrieveTaskSubmission.kt
+++ flipted/app/src/main/java/edu/calpoly/flipted/businesslogic/tasks/RetrieveTaskSubmission.kt
@@ -1,8 +1,7 @@
 package edu.calpoly.flipted.businesslogic.tasks
 
-import edu.calpoly.flipted.businesslogic.tasks.data.Task
 import edu.calpoly.flipted.businesslogic.tasks.data.TaskSubmissionResult
 
 class RetrieveTaskSubmission(private val repo : TasksRepo) {
-    suspend fun execute(taskId : String) : TaskSubmissionResult = repo.submitTask(taskId)
+    suspend fun execute(taskId : String) : TaskSubmissionResult = repo.retrieveTaskSubmission(taskId)
 }
