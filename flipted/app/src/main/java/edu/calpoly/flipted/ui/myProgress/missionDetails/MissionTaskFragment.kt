7c27a907e4e5511fe5a5f971d7db4221d519c385
Jordan Powers <jordanpowers1227@gmail.com>
2021-06-01 17:34:59 -0700

    Remove unused import

--- flipted/app/src/main/java/edu/calpoly/flipted/ui/myProgress/missionDetails/MissionTaskFragment.kt
+++ flipted/app/src/main/java/edu/calpoly/flipted/ui/myProgress/missionDetails/MissionTaskFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.commit
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import edu.calpoly.flipted.R
